fdac1d9160d0e520cb326e4a6b456bad7467ad8c
Wuke Zhang <ukesan2005@gmail.com>
2025-06-20 16:19:16 +0000

    fixed

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        _redirects: resolve(__dirname, 'public/_redirects')
-      }
-    }
+    outDir: 'dist'
   }
-});
+})
